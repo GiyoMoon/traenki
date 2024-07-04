037a7e0606d1d3d3fb7f35b0d314273bfa2a2592
Jasmin <code@jasi.dev>
2024-07-04 18:30:17 +0200

    feat: first version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
+import plugin from 'tailwindcss/plugin'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -12,10 +13,47 @@ export default {
         traenki: {
           dark: '#0B1214',
           neutral: '#F1ECE3',
-          primary: '#9DBAB9',
-          shade: '#3C5050',
+          primary: '#3C5050',
+          shade: '#9DBAB9',
+        },
+      },
+      animation: {
+        swap: 'swap 12s cubic-bezier(0.32,0.72,0,0.75) infinite',
+      },
+      keyframes: {
+        swap: {
+          '0%': {
+            opacity: 0,
+            transform: 'translateY(-50%)',
+          },
+          '2%': {
+            opacity: 1,
+            transform: 'translateY(0%)',
+          },
+          '16%': {
+            opacity: 1,
+            transform: 'translateY(0%)',
+          },
+          '18%': {
+            opacity: 0,
+            transform: 'translateY(50%)',
+          },
+          '100%': {
+            opacity: 0,
+            transform: 'translateY(50%)',
+          },
         },
       },
     },
   },
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.scrollbar-thin': {
+          'scrollbar-width': 'thin',
+          'scrollbar-color': '#9DBAB9 #F1ECE3',
+        },
+      })
+    }),
+  ],
 }
